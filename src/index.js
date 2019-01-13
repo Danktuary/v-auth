35f36da0b207c4d549a9121ca6989ee36ada5bb5
Sanctuary <sanc.pw@gmail.com>
2019-01-13 02:03:48 -0500

    Move store file to its own folder

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import Vue from 'vue';
+
 import App from './App.vue';
 import http from './http.js';
 import router from './router.js';
-import store from './store.js';
+import store from './store/index.js';
 
 Vue.prototype.$http = http;
 Vue.config.productionTip = false;
