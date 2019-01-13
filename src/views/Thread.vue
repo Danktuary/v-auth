e880f481e71fa2def027c595e678891736b88ef1
Sanctuary <sanc.pw@gmail.com>
2019-01-13 18:08:56 -0500

    Make Thread.vue a bit more functional

--- src/views/Thread.vue
+++ src/views/Thread.vue
@@ -1,10 +1,19 @@
 <template>
 	<div>
+		<h2>{{ thread.name }}</h2>
 		Thread ID #{{ thread.id }}. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta tenetur consequatur possimus iusto eveniet sequi minus rerum. Sequi corrupti illum non, voluptatibus minus atque consectetur officia animi aliquid ea excepturi.
 	</div>
 </template>
 
 <script>
+import http from '@/http.js';
+
+function handleError(error, next) {
+	const { message, response } = error;
+	console.error(message || (response && response.data.message));
+	next({ name: 'threads' });
+}
+
 export default {
 	name: 'Thread',
 
@@ -14,18 +23,29 @@ export default {
 		};
 	},
 
-	async mounted() {
+	async beforeRouteEnter(to, from, next) {
 		try {
-			const { data } = await this.$http.get(`/threads/${this.$route.params.id}`);
-			this.thread = data;
+			const { data } = await http.get(`/threads/${to.params.id}`);
+			next(vm => vm.thread = data);
 		} catch (error) {
-			if (error.response.status === 401) {
-				this.$router.push({ name: 'threads' });
-				return console.error(error.message || error.response.data.message);
-			}
+			handleError(error, next);
+		}
+	},
 
-			console.error(error);
+	async beforeRouteUpdate(to, from, next) {
+		try {
+			const { data } = await http.get(`/threads/${to.params.id}`);
+			this.thread = data;
+			next();
+		} catch (error) {
+			handleError(error, next);
 		}
 	},
 };
 </script>
+
+<style scoped>
+h2 {
+	margin: 0 0 1rem;
+}
+</style>
