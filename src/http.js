70707cb4994add6b362bc3e5160873165b839042
Sanctuary <sanc.pw@gmail.com>
2019-01-13 04:18:56 -0500

    Add http method to set auth header

--- src/http.js
+++ src/http.js
@@ -1,10 +1,16 @@
 import axios from 'axios';
 
-const token = localStorage.getItem('v-token');
+const { token } = JSON.parse(localStorage.getItem('v-user')) || {};
 const http = axios.create({ baseURL: 'http://localhost:4242' });
 
-if (token) {
-	http.defaults.headers.common.Authorization = `Bearer ${token}`;
-}
+http.setAuthHeader = function setAuthHeader(authToken) {
+	if (authToken) {
+		return this.defaults.headers.common.Authorization = `Bearer ${authToken}`;
+	}
+
+	delete this.defaults.headers.common.Authorization;
+};
+
+if (token) http.setAuthHeader(token);
 
 export default http;
