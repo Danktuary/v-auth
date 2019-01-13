ddec9dd13495b3ae4c838f3b5c17e197d69c3614
Sanctuary <sanc.pw@gmail.com>
2019-01-13 04:41:28 -0500

    Add dummy content to the homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,19 @@
 <template>
 	<div>
+		<template v-if="user && user.id">
+			Welcome back, {{ user.username }} (ID: {{ user.id }})
+		</template>
+		<template v-else>
+			Hey, it seems like you're not logged in! Why not <router-link to="/login">do that now</router-link>?
+		</template>
 	</div>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+
 export default {
 	name: 'Home',
+	computed: mapState(['user']),
 };
 </script>
